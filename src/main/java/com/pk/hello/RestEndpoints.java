7f7dd6154442d997b8118660aeb804ca1682e7b4
Pallav Kothari <pallavkothari@gmail.com>
2019-01-16 07:39:59 -0800

    print headers

--- src/main/java/com/pk/hello/RestEndpoints.java
+++ src/main/java/com/pk/hello/RestEndpoints.java
@@ -7,14 +7,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+import java.io.StringWriter;
+import java.util.Enumeration;
+
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 public class RestEndpoints {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
-        return "hello, world!";
+    public String index(HttpServletRequest request) {
+        Enumeration<String> headerNames = request.getHeaderNames();
+        StringWriter s = new StringWriter();
+        while (headerNames.hasMoreElements()) {
+            String header = headerNames.nextElement();
+            s.append(header).append("=").append(request.getHeader(header)).append("<br/>").append(System.lineSeparator());
+        }
+        return s.toString();
     }
 
     @ApiOperation(value = "greeting", nickname = "hi", produces = APPLICATION_JSON_VALUE)
